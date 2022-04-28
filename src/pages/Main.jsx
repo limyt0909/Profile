bd69391be736ef8959b32fa7cb3b771c453f7963
Lim Yong-tack <limyt0909@aonecustoms.com>
2022-04-28 17:43:49 +0900

    220428

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,5 +1,41 @@
+import "../scss/Main.scss";
 const Main = () => {
-  return <div>메인페이지</div>;
+  return (
+    <div class="container">
+      <svg
+        viewBox="0 0 1418 116"
+        version="1.1"
+        xmlns="http://www.w3.org/2000/svg"
+        // xmlns:xlink="http://www.w3.org/1999/xlink"
+      >
+        <g stroke="none" fill="none" fill-rule="evenodd" fill-opacity="0">
+          <text
+            id="@Dragon_Tack"
+            stroke="#fff"
+            fill="#645F5A"
+            font-weight="normal"
+            font-family="Nanum Pen Script"
+            font-size="144"
+          >
+            <tspan x="300" y="109">
+              <tspan>@</tspan>
+              <tspan>D</tspan>
+              <tspan>R</tspan>
+              <tspan>A</tspan>
+              <tspan>G</tspan>
+              <tspan>O</tspan>
+              <tspan>N</tspan>
+              <tspan>_</tspan>
+              <tspan>T</tspan>
+              <tspan>A</tspan>
+              <tspan>C</tspan>
+              <tspan>K</tspan>
+            </tspan>
+          </text>
+        </g>
+      </svg>
+    </div>
+  );
 };
 
 export default Main;
